cb0a2f9977e2f8ecac0e206026c78bc5860be605
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2024-07-16 18:54:28 -0300

    Initial commit

---
+++ src/main/java/br/com/rafadev/gestao_servidores_api/GestaoServidoresApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafadev.gestao_servidores_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoServidoresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoServidoresApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafadev/gestao_servidores_api/GestaoServidoresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafadev.gestao_servidores_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoServidoresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
